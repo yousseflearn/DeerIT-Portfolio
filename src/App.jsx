0cf586835a90fe66310a6cf233d3eabf35361e20
yousseflearn <youssefeasylearn@gmail.com>
2024-03-09 12:02:47 -0800

    Navbar Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <section>
-        <a href="">Hero</a>
+        <Navbar />
       </section>
       <section>Parallax</section>
       <section>Services</section>
