3d00bf95631a985d0552faa8f9ff5f7b58720471
yousseflearn <youssefeasylearn@gmail.com>
2024-03-09 04:53:12 -0800

    Create the project and configure it with sass

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <>
+      <section>
+        <a href="">Hero</a>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+    </>
+  );
+}
+
+export default App;
