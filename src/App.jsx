30e3b672f3f16b4944c399411512c6d5e9fe487b
yousseflearn <youssefeasylearn@gmail.com>
2024-03-11 15:40:56 -0700

    Animated Sidebar (Hamburger Animation)

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <>
-      <section>
+      <section id="Home">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Portfolio">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+      <section id="About">About</section>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import Sidebar from "../sidebar/Sidebar.jsx";
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar  */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Deer IT </span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Deer IT
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
