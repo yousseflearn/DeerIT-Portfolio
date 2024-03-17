b3c21ee70611f8a8ac97a2f54d6ed3589b4c97d0
yousseflearn <youssefeasylearn@gmail.com>
2024-03-17 04:42:28 -0700

    Create Animation For Hero Section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,27 +1,81 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-420%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "Mirror",
+      duration: 20,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>Deer IT</h2>
-          <h1>Web Development and UI Design</h1>
-          <div className="buttons">
-            <button>See The Latest Works</button>
-            <button>Contact us</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
-      </div>
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Deer IT</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web Development and UI Design
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See The Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact us</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt=""
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
 
-      <div className="slidingTextContainer">
-        Web and Mobile Application Development <br />
-        Networking and Telecominication <br />
-        Software and Hardware Services
-      </div>
-      <div className="imageContainer">
-        <img src="/hero.png" alt="" />
+        <motion.div
+          className="slidingTextContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate"
+        >
+          Web and Mobile Application Development Networking and Telecominication
+          Software and Hardware Services
+        </motion.div>
+        <div className="imageContainer">
+          <img src="/hero11.png" alt="" />
+        </div>
       </div>
     </div>
   );
